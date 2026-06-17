c8fba1004e9f30329eabb3eea93c04bc0dc60e76
Padminir07 <padhu9183@gmail.com>
2026-06-17 19:28:55 +0530

    Fixed search and embedding services

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -1,8 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-def generate_embeddings(chunks):
+# Load model once
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
-    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def generate_embeddings(chunks):
 
     texts = [chunk["text"] for chunk in chunks]
 
